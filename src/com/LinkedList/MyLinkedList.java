9f9ef3ef9bafdf7ef72b2cb15c1d2cc56220cdce
chethanjs97 <chethanjschethu@gmail.com>
2020-12-02 19:40:13 +0530

    Craated Linked List

--- src/com/LinkedList/MyLinkedList.java
+++ src/com/LinkedList/MyLinkedList.java
@@ -2,7 +2,7 @@ package com.LinkedList;
 
 public class MyLinkedList<K> {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Linked List Problem");
+		System.out.println("Welcome to  Linked List Problem");
 		MyNode<Integer> myFirstNode=new MyNode<Integer>(56);
 		MyNode<Integer> mySecondNode=new MyNode<Integer>(30);
 		MyNode<Integer> myThirdNode=new MyNode<	Integer>(70);	
