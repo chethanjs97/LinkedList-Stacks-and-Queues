12f3ea56231d75112cf372848dbad46d31233f08
chethanjs97 <chethanjschethu@gmail.com>
2020-12-02 19:37:48 +0530

    Welcome to linked List

---
+++ src/com/LinkedList/MyLinkedList.java
@@ -0,0 +1,11 @@
+package com.LinkedList;
+
+public class MyLinkedList<K> {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Linked List Problem");
+		MyNode<Integer> myFirstNode=new MyNode<Integer>(56);
+		MyNode<Integer> mySecondNode=new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode=new MyNode<	Integer>(70);	
+	}
+
+}
